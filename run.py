46bd9c85abaa90eb0270a6a14462685fa72abecd
Raksha Muthukumar <raksha@Rakshas-MacBook-Pro.local>
2022-10-01 18:13:30 -0400

    added preliminary templates

--- run.py
+++ run.py
@@ -2,6 +2,11 @@
 #export FLASK_ENV=development
 #flask run
 
+# So far we have:
+#   1) Starting page
+#   2) Questions for landlords (2 pages)
+#   3) Profile view of a tenant
+
 #!/venv/bin/python
 from flask import Flask, render_template
 
@@ -18,12 +23,16 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-#localhost:5000/landlord/john
-@app.route('/landlord/<name>')
+#localhost:5000/landlord
+@app.route('/landlord')
+def landlord():
+    return render_template("landlord_first_page.html")
 
-def landlord(name):
-    return '<h1>Hello {}</h1>'.format(name)
-    #return db.landlord_full_name()
+#localhost:5000/tenant
+@app.route('/tenant/<tenant_name>')
+def tenant(tenant_name):
+    return render_template("tenant_profile_page.html",
+        tenant_name=tenant_name)
 
 # Keep this at the bottom of run.py
 app.run(debug=True)
